4756fcf6b09cf0f2b28ed183653ca9667408f459
Ruth Harris <ruth@ruthharris.me>
2022-12-26 01:44:16 -0500

    a lot of changes
    but it's still just a landing page. (no click to meow yet)

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,21 @@
 const htmlmin = require('html-minifier')
-const svgContents = require("eleventy-plugin-svg-contents");
+const svgContents = require("eleventy-plugin-svg-contents")
+const metagen = require('eleventy-plugin-metagen')
 
 const now = String(Date.now())
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPlugin(svgContents);
+	eleventyConfig.addPlugin(svgContents)
+	eleventyConfig.addPlugin(metagen)
 
 	eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
 	eleventyConfig.addWatchTarget('./styles/tailwind.css')
+
 	eleventyConfig.addPassthroughCopy({
 		'./_tmp/style.css': './style.css'
 	})
 	eleventyConfig.addPassthroughCopy('./assets/')
+	eleventyConfig.addPassthroughCopy('./favicon.ico')
 
 	eleventyConfig.addShortcode('version', function () {
 		return now
@@ -32,4 +36,5 @@ module.exports = function (eleventyConfig) {
 		}
 		return content
 	})
-};
+
+}
---
+++ _data/eleventyComputed.js
@@ -0,0 +1,20 @@
+module.exports = {
+	metadata: {
+		socials: data => {
+			var s = Array.from(data.metadata.socials)
+			s.forEach(network => {
+				const serviceData = data.socialnetworks[network.service]
+				network.url = serviceData.baseUrl + network.handle
+				if (serviceData.canonicalName == "Mastodon") {
+					// If we have a leading @, we might start with a blank entry in the array, so filter that out
+					const components = network.handle.split("@").filter(x => x.length > 0)
+					network.url = serviceData.baseUrl + components[1] + "/" + components[0]
+				}
+				network.altText = serviceData.prefix + network.handle + " on " + serviceData.canonicalName
+				network.svgUrl = serviceData.img
+			})
+			return s
+		}
+	},
+	twitterHandle: data => data.metadata.socials.find(x => (x.service == "twitter"))["handle"]
+}
